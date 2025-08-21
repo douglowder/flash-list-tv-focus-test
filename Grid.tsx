ff6b9a850391d0e8fecf5fe55484f244eb7d0139
Douglas Lowder <douglowder@mac.com>
2025-08-21 12:04:15 -0700

    Revert "Fix for issue"
    This reverts commit 9fa09fc926c49d773534cfa8ffcae5dce6934656.

--- Grid.tsx
+++ Grid.tsx
@@ -49,7 +49,6 @@ const Grid = () => {
           index={index}
           onFocus={handleFocus}
           onPress={updateStrings}
-          key={item.id}
         >
           <View
             style={{
@@ -72,6 +71,7 @@ const Grid = () => {
       data={state}
       drawDistance={200}
       estimatedItemSize={50}
+      keyExtractor={keyExtractor}
       renderItem={handleRenderRow}
       scrollEnabled={false}
       showsHorizontalScrollIndicator={false}
